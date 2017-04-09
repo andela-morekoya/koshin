336161cc539ea16671f30a4970146ef0d53dd9b2
Chukwuma Ezumezu <chukwuma@Chukwumas-MacBook-Pro.local>
2017-04-09 22:49:20 +0100

    refactor app

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import logo from './logo.svg';
+import './styles/index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
