3d51684c07cacde5808b7be600f9216adb3ed877
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2017-04-16 15:15:10 +0100

    create models

---
+++ server/models/repo.js
@@ -0,0 +1,40 @@
+module.exports = (sequelize, DataTypes) => {
+  const Repo = sequelize.define('Repo', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      unique: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'name',
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      field: 'url',
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      field: 'description',
+    },
+    lastReportDate: {
+      type: DataTypes.DATE,
+      field: 'last_report_date',
+    },
+    defaultReportBranch: {
+      type: DataTypes.STRING,
+      field: 'default_report_branch',
+    },
+    report: {
+      type: DataTypes.BOOLEAN,
+    },
+  },
+    {
+      timestamps: true,
+      tableName: 'repos',
+    });
+  return Repo;
+};
