f71ce7095c99ccef9281275c965c86fe5fa629d7
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2017-04-22 10:23:21 +0100

    refcator action creations

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -3,7 +3,4 @@ import rootReducer from '../reducers';
 import reactImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 
-const initialState = {
-};
-
-export default createStore(rootReducer, initialState);
+export default createStore(rootReducer, applyMiddleware(thunk));
