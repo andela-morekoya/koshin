e56e2ea4d45ef2653cb5da50113a4da96c1bebd5
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2017-04-22 10:24:09 +0100

    choose what to render on logged in

---
+++ client/src/Main.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Login from './components/Login';
+import Home from './components/Home';
+import { connect, Provider } from 'react-redux';
+
+function redirect(path) {
+  window.location.href = path || '/';
+}
+
+class Main extends React.Component {
+  render() {
+    const user = this.props.user;
+    return (
+      <div>
+        {
+          user.id ?
+            <Home />
+            :
+            <Login />
+        }
+      </div>
+    );
+  }
+}
+
+Main.propTypes = {
+  user: React.PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    user: state.user.data
+  };
+}
+
+export default connect(mapStateToProps)(Main);
--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,13 +1,47 @@
-import React, {PropTypes} from 'react';
-import {Link, IndexLink} from 'react-router';
+import React, { PropTypes } from 'react';
+import { Link, IndexLink } from 'react-router';
+import { connect } from 'react-redux';
 
-const Header = () => {
-  return (<nav>
-            <IndexLink to="/" activeClassName="active">Home</IndexLink>
-            {" | "}
-            <a href="/logout">Logout</a>            
-          </nav>
-  );
-};
+class Header extends React.Component {
+  renderOnLogin() {
+    const user = this.props.user;
+    return (
+      <div style={{ float: 'right' }}>
+        <div style={{ verticalAlign: 'bottom', display: 'inline-block' }}>
+        <a href="/logout">Logout</a>
+        <span>{user.name}</span>
+        </div>
+        <img style={{ height: '80%' }} src={user.avatar_url} alt="LoggedIn user avater" />
+      </div>
+    )
+  }
+  render() {
+    const user = this.props.user;
+    return (
+      <nav className="navbar navbar-default" style={{ height: '50px' }}>
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">Koshin</a>
+          </div>
+          {
+            user && user.name ?
+              this.renderOnLogin()
+              : ''
+          }
+        </div>
+      </nav>
+    );
+  }
+}
 
-export default Header;
+Header.propTypes = {
+  user: React.PropTypes.object.isRequired
+}
+
+function mapStateToProps(state) {
+  return {
+    user: state.user.data
+  }
+}
+
+export default connect(mapStateToProps)(Header);
