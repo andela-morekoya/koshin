4d4085240bfe891f89be3f4a571a6289eb071392
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2017-04-30 19:19:35 +0100

    show addrepo when user has no watched repo

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -6,65 +6,46 @@ import OrgRepo from './OrgRepo';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-class Home extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      tabs: {
-        one: { head: '', body: 'tab-pane fade' },
-        three: { head: 'active', body: 'tab-pane fade in active' }
-      }
-    };
-    this.setActiveTab = this.setActiveTab.bind(this);
-  }
+const Home = (props) => {
+  const tabOneHead = props.watchedRepos.length < 1 ? 'active' : '';
+  const tabThreeHead = props.watchedRepos.length > 0 ? 'active' : '';
 
-  setActiveTab() {
-    const tabs = this.state.tabs;
-    if (!this.props.watchedRepos.length) {
-      tabs.one.head = 'active';
-      tabs.one.body = 'tab-pane fade in active';
-      tabs.three.head = '';
-      tabs.three.body = 'tab-pane fade';
-    }
-    this.setState({ tabs });
-  }
+  const tabOneBody = props.watchedRepos.length < 1 ? 'in active' : '';
+  const tabThreeBody = props.watchedRepos.length > 0 ? 'in active' : '';
 
-  render() {
-    const tabs = this.state.tabs;
-    return (
-      <div className="container-fluid container-fluid-main">
-        <div className="row">
-          <div className="col-md-3">
-            <WatchedRepos />
-          </div>
-          <div className="col-md-9">
-            <Link to="/settings">
+  return (
+    <div className="container-fluid container-fluid-main">
+      <div className="row">
+        <div className="col-md-3">
+          <WatchedRepos />
+        </div>
+        <div className="col-md-9">
+          <Link to="/settings">
             <div style={{ float: 'right', fontSize: '18px', cursor: 'pointer' }} >
               Settings <i className="fa fa-cog" aria-hidden="true" style={{ fontSize: '30px', verticalAlign: 'middle' }}></i>
             </div>
-            </Link>
-            <ul className="nav nav-tabs">
-              <li className={tabs.one.head} ><a data-toggle="tab" href="#your-repo">Your Repositories</a></li>
-              <li><a data-toggle="tab" href="#org-repo">Organisation Repos</a></li>
-              <li className={tabs.three.head}><a data-toggle="tab" href="#report">Reports</a></li>
-            </ul>
-            <div className="tab-content">
-              <div id="your-repo" className={tabs.one.body}>
-                <PersonalRepo />
-              </div>
-              <div id="org-repo" className="tab-pane fade">
-                <OrgRepo />
-              </div>
-              <div id="report" className={tabs.three.body}>
-                <Report />
-              </div>
+          </Link>
+          <ul className="nav nav-tabs">
+            <li className={tabOneHead} ><a data-toggle="tab" href="#your-repo">Your Repositories</a></li>
+            <li><a data-toggle="tab" href="#org-repo">Organisation Repos</a></li>
+            <li className={tabThreeHead}><a data-toggle="tab" href="#report">Reports</a></li>
+          </ul>
+          <div className="tab-content">
+            <div id="your-repo" className={`tab-pane fade ${tabOneBody}`}>
+              <PersonalRepo />
+            </div>
+            <div id="org-repo" className="tab-pane fade">
+              <OrgRepo />
+            </div>
+            <div id="report" className={`tab-pane fade ${tabThreeBody}`}>
+              <Report />
             </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Home.propTypes = {
   watchedRepos: React.PropTypes.array.isRequired
